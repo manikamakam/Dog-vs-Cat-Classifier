fc207f8555165a8b23132f29a84318d76bc99aa0
p-akanksha <p.akanksha94@gmail.com>
2020-05-16 20:47:43 -0400

    added code to generate new folder

--- data_format.py
+++ data_format.py
@@ -10,12 +10,26 @@ val_ratio = 0.25
 
 dataset_home='data/'
 src_directory = 'data/train1/'
+
+if not os.path.exists("data/train"):
+    os.makedirs("data/train")
+if not os.path.exists("data/train/cats/"):
+    os.makedirs("data/train/cats/")
+if not os.path.exists("data/train/dogs/"):
+    os.makedirs("data/train/dogs/")
+
+if not os.path.exists("data/val"):
+    os.makedirs("data/val")
+if not os.path.exists("data/val/cats/"):
+    os.makedirs("data/val/cats/")
+if not os.path.exists("data/val/dogs/"):
+    os.makedirs("data/val/dogs/")
 for file in listdir(src_directory):
     print(file)
     src = src_directory + '/' + file
     dst_dir = 'train/'
     if random() < val_ratio:
-        dst_dir = 'test/'
+        dst_dir = 'val/'
     if file.startswith('cat'):
         dst = dataset_home + dst_dir + 'cats/'  + file
         copyfile(src, dst)
